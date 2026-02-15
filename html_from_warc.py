e7cb2bd078a34f9a76560821c40c78194b37ed07
jithesh82 <jithesh82@gmail.com>
2026-02-15 16:21:32 -0500

    update html_from_warc.py

--- html_from_warc.py
+++ html_from_warc.py
@@ -1,13 +1,14 @@
 from warcio.archiveiterator import ArchiveIterator
 import gzip
 
-target = "https://www.underarmour.com/en-us/c/mens/sports/golf/green+purple+yellow-clothing/"
+#target = "https://www.underarmour.com/en-us/c/mens/sports/golf/green+purple+yellow-clothing/"
+target = "http://www.underarmour.com/"
 
-with gzip.open("CC-MAIN-20240304133241-20240304163241-00450.warc.gz", "rb") as stream:
+with gzip.open("CC-MAIN-20260114060053-20260114090053-00556.warc.gz", "rb") as stream:
     for record in ArchiveIterator(stream):
         if record.rec_type == "response":
-            if record.rec_headers.get_header("WARC-Target-URI") == target:
+            #if record.rec_headers.get_header("WARC-Target-URI") == target:
                 payload = record.content_stream().read()
                 print(payload.decode("utf-8", errors="ignore"))
-                with open('testhtml.html', 'w') as f:
+                with open('CC-MAIN-20260114060053-20260114090053-00556.warc.gz.html', 'w') as f:
                     f.write(payload.decode("utf-8", errors="ignore"))
