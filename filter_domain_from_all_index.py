24250cae091ec4af0ab45a51bb315ee8924a5e59
jithesh82 <jithesh82@gmail.com>
2026-02-15 15:44:29 -0500

    update filter_domain_from_all_index.py - logic to download warc

--- filter_domain_from_all_index.py
+++ filter_domain_from_all_index.py
@@ -1,21 +1,35 @@
 # coding: utf-8
-import sys
-allindexfile = sys.argv[1]
-outputfile = sys.argv[2]
-domain = sys.argv[3]
+"""
+this program reads common crawl all index file;  loops through it; parases it; download the warc file
+"""
+import os
+
+m = 0
 s=""
-with open(allindexfile) as inf:
-    with open(outputfile, 'w') as ouf:
-        lines = inf.readlines()
-        for line in lines:
-            #line = eval(line)
-            if domain in line:
-                #print(line)
-                s += line.strip()
-                s += ','
-            #if len(s) > 1000: 
-            #    break
-        s = s.rstrip(',')
-        s = '[' + s + ']'
-        ouf.write(s)
-        
+with open('underarmour_all_index.txt') as inf:
+    with open('filtered_underarmour_all_index.txt', 'w') as ouf:
+        #lines = inf.readline()
+        for line in inf:
+            m += 1
+            if m>=1:
+                #line = eval(line)
+                if "www.underarmour.com" in line:
+                    print(line)
+                    #s += line.strip()
+                    #s += ','
+                    line = eval(line)
+                    print(type(line))
+                    filename = line['filename']
+                    offset = line['offset']
+                    length = line['length']
+                    print(filename, offset, length)
+                if m == 10:
+                    break
+            
+start = int(offset)
+end = start + int(length) - 1
+baseurl = "https://data.commoncrawl.org/"
+(_, warcfile) = os.path.split(filename)
+outputdir = "warcdir"
+downloadurl = f'curl -H \"Range: bytes={start}-{end}\" \"{baseurl + filename}\" --output \"{outputdir}/{warcfile}\"'
+os.system(downloadurl)
