a951166845d57534725641c9046175dc82bb6511
jithesh82 <jithesh82@gmail.com>
2026-03-20 20:57:32 -0400

    update download_domain_from_all_index.py

--- download_domain_from_all_index.py
+++ download_domain_from_all_index.py
@@ -8,11 +8,11 @@ import time
 m = 0
 s=""
 with open('underarmour_all_index.txt') as inf:
-    with open('filtered_underarmour_all_index.txt', 'w') as ouf:
+    #with open('filtered_underarmour_all_index.txt', 'w') as ouf:
         #lines = inf.readline()
         for line in inf:
             m += 1
-            if m>=101:
+            if m>=15924:
                 #line = eval(line)
                 if "www.underarmour.com" in line:
                     print(line)
@@ -25,16 +25,16 @@ with open('underarmour_all_index.txt') as inf:
                     length = line['length']
                     status = line['status']
                     print(filename, offset, length)
-                if m == 200:
-                    break
-                if status == '418':
-                    continue
+                    if m > 20000:
+                        break
+                    if status == '418':
+                        continue
             
-                start = int(offset)
-                end = start + int(length) - 1
-                baseurl = "https://data.commoncrawl.org/"
-                (_, warcfile) = os.path.split(filename)
-                outputdir = "warcdir"
-                downloadurl = f'curl -H \"Range: bytes={start}-{end}\" \"{baseurl + filename}\" --output \"{outputdir}/{warcfile}\"'
-                os.system(downloadurl)
-                time.sleep(3)
+                    start = int(offset)
+                    end = start + int(length) - 1
+                    baseurl = "https://data.commoncrawl.org/"
+                    (_, warcfile) = os.path.split(filename)
+                    outputdir = "warcdir"
+                    downloadurl = f'curl -H \"Range: bytes={start}-{end}\" \"{baseurl + filename}\" --output \"{outputdir}/{warcfile}\"'
+                    os.system(downloadurl)
+                    time.sleep(3)
