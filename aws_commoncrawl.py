5dfa7d3b8cc6cd933017944a919c9afbb220083c
jithesh82 <jithesh82@gmail.com>
2026-03-27 17:41:55 -0400

    update aws_commoncrawl.py

--- aws_commoncrawl.py
+++ aws_commoncrawl.py
@@ -40,6 +40,29 @@ dfhosts = pd.read_csv('./athena-9ecf898d-29ee-4f25-a188-f6581aa993a1.csv')
 print(dfhosts.head(10))
 dfhosts = dfhosts.head(1)
 
+async def fetch_warc(s3client, row):
+    url = row['url']
+    warc_path = row['warc_filename']
+    offset = int(row['warc_record_offset'])
+    length = int(row['warc_record_length'])
+    rangereq = 'bytes={}-{}'.format(offset, (offset+length-1))
+    response = await s3client.get_object(Bucket='commoncrawl', Key=warc_path, Range=rangereq, RequestPayer='requester')
+    body_data = await response['Body'].read()
+    return body_data
+
+def parse_html(data):
+    record_stream = BytesIO(data)
+    for record in ArchiveIterator(record_stream):
+        if record.rec_type == 'response':
+            warc_target_uri = record.rec_headers.get_header('WARC-Target-URI')
+            page = record.content_stream().read()
+            soup = BeautifulSoup(page, 'html.parser') 
+            title = soup.title.string if soup.title else ''
+            links = [link.get('href') for link in soup.find_all('a')]
+            comments = [comment for comment in soup.find_all(text=lambda text: isinstance(text, Comment))]
+            return links, comments, title
+    return [], [], ''
+
 # Thank you to Sebastian Nagel for your instructions and code to perform the following step.
 # http://netpreserve.org/ga2019/wp-content/uploads/2019/07/IIPCWAC2019-SEBASTIAN_NAGEL-Accessing_WARC_files_via_SQL-poster.pdf
 titles_list = []
@@ -60,94 +83,63 @@ def processwarcrecords(dfhosts, writefiles, searchfiles, howmanyrecords):
     if howmanyrecords == 0:
         howmanyrecords = totalrecords
     async def analyzeDFRows(index, row, semaphore, db, s3client):
-        # async with aiosqlite.connect('db_straylight.db') as db:
-            # async with session.client("s3") as s3client:
-                # for index, row in dfhosts.iterrows():
-                async with semaphore:
-                    # if recordcount > howmanyrecords:
-                    #     break
-                    nonlocal recordcount, skippedrecords, processedrecords, processedrows
-                    recordcount = recordcount + 1
-                    processedrows = processedrows + 1
-                    #print('Processing row ' + str(recordcount) + 
-                    #    ' of ' + str(totalrecords) + ' total rows.')
-                    #print('Processed ' + str(processedrecords) + ' records.')
-                    url = row['url']
-                    warc_path = row['warc_filename']
-                    offset = int(row['warc_record_offset'])
-                    length = int(row['warc_record_length'])
-                    rangereq = 'bytes={}-{}'.format(offset, (offset+length-1))
-                    times3_start = time.perf_counter()
-                    response = await s3client.get_object(Bucket='commoncrawl',
-                                                Key=warc_path,Range=rangereq, RequestPayer='requester')
-                    times3_end = time.perf_counter()
-                    print("s3 get time: %.2f" % (times3_end - times3_start))
-                    body_data = await response['Body'].read()
-                    record_stream = BytesIO(body_data)
-                    for record in ArchiveIterator(record_stream):
-                        tmptitles_list = []
-                        tmplinks_list = []
-                        tmpcomments_list = []
-                        if record.rec_type == 'response':
-                            try:
-                                warc_target_uri = record.rec_headers.get_header('WARC-Target-URI')
-
-                                page = record.content_stream().read()
-                                # lxml should be faster but is not
-                                timebs_start = time.perf_counter()
-                                soup = BeautifulSoup(page, 'html.parser') 
-                                timebs_end = time.perf_counter()
-                                print("time bs: %.2f" % (timebs_end - timebs_start))
-                                title = soup.title.string
-                                # titles_list.append((warc_target_uri, title))
-                                # timedbins_start = time.perf_counter()
-                                # await db.execute('''INSERT INTO titles (url, title) VALUES (?, ?)''', (warc_target_uri, title))
-                                # await db.commit()
-                                tmptitles_list.append((warc_target_uri, title))
-                                # timedbins_end = time.perf_counter()
-                                # print("time db insert: %.2f", (timedbins_end - timedbins_start))
-                                # uris_list.append((warc_target_uri))
-                                if searchfiles == 'yes':
-                                    # Find all links
-                                    timelinks_start = time.perf_counter()
-                                    for link in soup.find_all('a'):
-                                        # links_list.append((warc_target_uri, link.get('href')))
-                                        tmplinks_list.append((warc_target_uri, link.get('href')))
-                                    timelinks_end = time.perf_counter()
-                                    print("time links: %.2f" % (timelinks_end - timelinks_start))
-                                    # Find all comments
-                                    timecomments_start = time.perf_counter()
-                                    for comment in soup.find_all(text=lambda text: isinstance(text, Comment)):
-                                        # comments_list.append((warc_target_uri, comment))
-                                        tmpcomments_list.append((warc_target_uri, comment))
-                                    timecomments_end = time.perf_counter()
-                                    print("time comments: %.2f" % (timecomments_end - timecomments_start))
-                                #print('Found title: ' + title)
-                                #print('Found ' + str(len(links_list)) + ' links so far.')
-                                #print('Found ' + str(len(comments_list)) + ' comments so far.')
-                                print("waiting for db executemany...   ")
-                                timedbexecmany_start = time.perf_counter()
-                                await db.executemany('''INSERT INTO titles (url, title) VALUES (?, ?)''', tmptitles_list)
-                                await db.executemany('''INSERT INTO links (url, link) VALUES (?, ?)''', tmplinks_list)
-                                await db.executemany('''INSERT INTO comments (url, comment) VALUES (?, ?)''', tmpcomments_list) 
-                                await db.commit()
-                                timedbexecmany_end = time.perf_counter()
-                                print("time db executemany: %.2f" % (timedbexecmany_end - timedbexecmany_start))
-                                if writefiles == 'yes':
-                                    page = page.decode("utf-8") 
-                                    url = url.replace("https://","")
-                                    url = url.replace("http://","")
-                                    url = url + str(offset) + '.html'
-                                    filepath = os.getcwd() + '/tmp/' + url
-                                    os.makedirs(os.path.dirname(filepath), exist_ok=True)
-                                    with open(filepath, "w") as text_file:
-                                        text_file.write(soup.prettify())
-                                        processedrecords = processedrecords + 1
-                            except Exception as e:
-                                logger = logging.getLogger('errorhandler')
-                                print(logger.error('Error: '+ str(e)))
-                                skippedrecords = skippedrecords + 1
-                                print('Skipped ' + str(skippedrecords) + ' records.')
+        async with semaphore:
+            try:
+                nonlocal recordcount, skippedrecords, processedrecords, processedrows
+                recordcount = recordcount + 1
+                processedrows = processedrows + 1
+                url = row['url']
+                warc_path = row['warc_filename']
+                offset = int(row['warc_record_offset'])
+                length = int(row['warc_record_length'])
+                rangereq = 'bytes={}-{}'.format(offset, (offset+length-1))
+                times3_start = time.perf_counter()
+                response = await s3client.get_object(Bucket='commoncrawl',
+                                            Key=warc_path,Range=rangereq, RequestPayer='requester')
+                times3_end = time.perf_counter()
+                print("s3 get time: %.2f" % (times3_end - times3_start))
+                body_data = await response['Body'].read()
+                record_stream = BytesIO(body_data)
+                for record in ArchiveIterator(record_stream):
+                    tmptitles_list = []
+                    tmplinks_list = []
+                    tmpcomments_list = []
+                    if record.rec_type == 'response':
+                            warc_target_uri = record.rec_headers.get_header('WARC-Target-URI')
+                            page = record.content_stream().read()
+                            # lxml should be faster but is not
+                            timebs_start = time.perf_counter()
+                            soup = BeautifulSoup(page, 'html.parser') 
+                            timebs_end = time.perf_counter()
+                            print("time bs: %.2f" % (timebs_end - timebs_start))
+                            title = soup.title.string
+                            tmptitles_list.append((warc_target_uri, title))
+                            if searchfiles == 'yes':
+                                # Find all links
+                                timelinks_start = time.perf_counter()
+                                for link in soup.find_all('a'):
+                                    tmplinks_list.append((warc_target_uri, link.get('href')))
+                                timelinks_end = time.perf_counter()
+                                print("time links: %.2f" % (timelinks_end - timelinks_start))
+                                # Find all comments
+                                timecomments_start = time.perf_counter()
+                                for comment in soup.find_all(text=lambda text: isinstance(text, Comment)):
+                                    tmpcomments_list.append((warc_target_uri, comment))
+                                timecomments_end = time.perf_counter()
+                                print("time comments: %.2f" % (timecomments_end - timecomments_start))
+                            print("waiting for db executemany...   ")
+                            timedbexecmany_start = time.perf_counter()
+                            await db.executemany('''INSERT INTO titles (url, title) VALUES (?, ?)''', tmptitles_list)
+                            await db.executemany('''INSERT INTO links (url, link) VALUES (?, ?)''', tmplinks_list)
+                            await db.executemany('''INSERT INTO comments (url, comment) VALUES (?, ?)''', tmpcomments_list) 
+                            await db.commit()
+                            timedbexecmany_end = time.perf_counter()
+                            print("time db executemany: %.2f" % (timedbexecmany_end - timedbexecmany_start))
+            except Exception as e:
+                logger = logging.getLogger('errorhandler')
+                print(logger.error('Error: '+ str(e)))
+                skippedrecords = skippedrecords + 1
+                print('Skipped ' + str(skippedrecords) + ' records.')
 
     async def worker(queue, index, semaphore, db, s3client):
         while True:
